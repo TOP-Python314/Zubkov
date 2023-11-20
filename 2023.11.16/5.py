2012614524c49d9574c6534e01d87b79a01d630b
максим <908810185a@gmail.com>
2023-11-20 13:38:54 +0300

    домашняя работа от 16.11.2023

---
+++ 2023.11.16/5.py
@@ -0,0 +1,19 @@
+integer = int(input())
+fraction = input()
+result = integer + (float(str('0.'+ fraction)))
+
+print(f'{result} миль = {result * 1.61 :.1f}')
+
+
+# 15
+# 7
+# 15.7 миль = 25.3
+
+
+# 10
+# 25
+# 10.25 миль = 16.5
+
+# 23
+# 234
+# 23.234 миль = 37.4
